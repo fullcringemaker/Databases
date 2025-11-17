6c74449d8672f852657948c8e05a547d74c83658
Дмитрий <dmitriytrofimenko105@mail.ru>
2025-11-18 02:52:36 +0300

    Update lab6.sql

--- Lab6/lab6.sql
+++ Lab6/lab6.sql
@@ -32,3 +32,43 @@ USE Lab6;
 GO
 
 -- Task 1:
+IF OBJECT_ID('dbo.AIRCRAFT', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.AIRCRAFT
+    (
+        AircraftID INT PRIMARY KEY IDENTITY(1,1),
+        BoardNumber VARCHAR(8) NOT NULL,
+        Model NVARCHAR(10) NOT NULL,
+        Manufacturer NVARCHAR(20) NOT NULL,
+        PassengerCapacity SMALLINT NOT NULL,
+        LoadCapacity NUMERIC(6,2) NOT NULL,
+        AircraftAge TINYINT NOT NULL,
+        Status TINYINT NOT NULL,
+        CONSTRAINT AK_BoardNumber UNIQUE (BoardNumber)
+    );
+END;
+GO
+
+-- Task 2:
+IF OBJECT_ID('dbo.FLIGHT', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.FLIGHT
+    (
+        FlightID INT PRIMARY KEY IDENTITY(1,1),
+        FlightNumber VARCHAR(5) NOT NULL,
+        FlightDate Date NOT NULL,
+        Airline CHAR(2) NOT NULL,
+        DepartureAirport CHAR(3) NOT NULL,
+        ArrivalAirport CHAR(3) NOT NULL,
+        BoardingTime DATETIME NOT NULL,
+        DepartureTime DATETIME NOT NULL,
+        ArrivalTime DATETIME NOT NULL,
+        Status TINYINT NOT NULL DEFAULT 1 CHECK(Status > 0 AND Status < 6),
+        AircraftID INT NOT NULL,
+        CONSTRAINT AK_Flight UNIQUE (FlightNumber, FlightDate),
+        CONSTRAINT FK_AircraftID FOREIGN KEY (AircraftID) REFERENCES dbo.AIRCRAFT(AircraftID)
+    );
+END;
+GO
+
+-- Task 3:
