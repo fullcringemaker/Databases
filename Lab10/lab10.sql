5bfb42e9fe330e70dc2da4f6542f4af883f2cdd3
Дмитрий <dmitriytrofimenko105@mail.ru>
2025-12-02 15:22:12 +0300

    Update lab10.sql

--- Lab10/lab10.sql
+++ Lab10/lab10.sql
@@ -1,1 +1,31 @@
+-- Лабораторная работа 10
+USE master;
+GO
 
+IF DB_ID(N'Lab10') IS NOT NULL
+BEGIN
+    ALTER DATABASE Lab10 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Lab10;
+END
+GO
+
+CREATE DATABASE Lab10
+ON PRIMARY(
+    NAME = Lab10_dat,
+    FILENAME = 'D:\database\lab10\Lab10_dat.mdf', 
+    SIZE = 10MB,
+    MAXSIZE = UNLIMITED, 
+    FILEGROWTH = 5%
+)
+LOG ON (NAME = Lab_10log,
+    FILENAME = 'D:\database\lab10\Lab10_log.ldf',
+    SIZE = 5MB,
+    MAXSIZE = 25MB,
+    FILEGROWTH = 5MB
+);
+GO
+
+USE Lab10;
+GO
+
+--Task 1:
