5dddbacb85a1908f8ca9ac702ea8ac05c5dc6ee6
Дмитрий <dmitriytrofimenko105@mail.ru>
2025-12-03 02:19:19 +0300

    Update lab9.sql

--- Lab9/lab9.sql
+++ Lab9/lab9.sql
@@ -47,6 +47,7 @@ IF OBJECT_ID(N'Trigger_Insert_AIRCRAFT') IS NOT NULL
     DROP TRIGGER Trigger_Insert_AIRCRAFT;
 GO
 
+-- Insert
 CREATE TRIGGER Trigger_Insert_AIRCRAFT
 ON AIRCRAFT
 AFTER INSERT
@@ -62,19 +63,19 @@ IF OBJECT_ID(N'Trigger_Update_AIRCRAFT') IS NOT NULL
     DROP TRIGGER Trigger_Update_AIRCRAFT;
 GO
 
+-- Update
 CREATE TRIGGER Trigger_Update_AIRCRAFT
 ON dbo.AIRCRAFT
 AFTER UPDATE
 AS
 BEGIN
-    -- Проверяем, что длина BoardNumber >= 4
     IF EXISTS (
         SELECT 1
         FROM inserted
         WHERE LEN(BoardNumber) < 4
     )
     BEGIN
-        RAISERROR(N'BoardNumber must contain at least 4 characters.', 16, 1);
+        RAISERROR(N'BoardNumber must contain at least 4 characters', 16, 1);
         ROLLBACK TRANSACTION;
         RETURN;
     END;
@@ -88,6 +89,7 @@ IF OBJECT_ID(N'Trigger_Delete_AIRCRAFT') IS NOT NULL
     DROP TRIGGER Trigger_Delete_AIRCRAFT;
 GO
 
+-- Delete
 CREATE TRIGGER Trigger_Delete_AIRCRAFT
 ON AIRCRAFT
 AFTER DELETE
@@ -97,6 +99,39 @@ BEGIN
     SELECT *
     FROM deleted;
 END;
+GO
+
+-- Tests:
+INSERT INTO AIRCRAFT (BoardNumber, Model, Manufacturer)
+VALUES 
+    ('ABCD1234', 'A320', 'Airbus'),
+    ('EFGH5678', 'B737', 'Boeing');
+GO
+
+SELECT * FROM AIRCRAFT;
+GO
+
+UPDATE AIRCRAFT
+SET BoardNumber = 'QWER9999'
+WHERE BoardNumber = 'ABCD1234';
+GO
+
+SELECT * FROM AIRCRAFT;
+GO
+
+--invalid
+--UPDATE AIRCRAFT
+--SET BoardNumber = 'AA'
+--WHERE BoardNumber = 'EFGH5678';
+--GO
+--SELECT * FROM AIRCRAFT;
+--GO
+
+DELETE FROM AIRCRAFT
+WHERE BoardNumber = 'QWER9999';
+GO
+
+SELECT * FROM AIRCRAFT;
 GO
 
 -- Task 2:
@@ -115,11 +150,11 @@ CREATE TABLE AircraftTechSpecifications
 );
 GO
 
-IF OBJECT_ID(N'view_AIRCRAFTAndTechSpecification') IS NOT NULL
-    DROP TABLE view_AIRCRAFTAndTechSpecification;
+IF OBJECT_ID(N'view_AIRCRAFTAndTechSpecifications') IS NOT NULL
+    DROP TABLE view_AIRCRAFTAndTechSpecifications;
 GO
 
-CREATE VIEW view_AIRCRAFTAndTechSpecification
+CREATE VIEW view_AIRCRAFTAndTechSpecifications
 AS
     SELECT
         a.AircraftID,
@@ -132,7 +167,200 @@ AS
         ts.Status
     FROM
         AIRCRAFT a
-        INNER JOIN
-        AircraftTechSpecifications ts ON a.AircraftID = ts.AircraftID;
+        INNER JOIN AircraftTechSpecifications ts 
+            ON a.AircraftID = ts.AircraftID;
 GO
 
+IF OBJECT_ID(N'Trigger_Insert_view_AIRCRAFTAndTechSpecifications') IS NOT NULL 
+    DROP TRIGGER Trigger_Insert_view_AIRCRAFTAndTechSpecifications;
+GO
+
+CREATE TRIGGER Trigger_Insert_view_AIRCRAFTAndTechSpecifications
+ON view_AIRCRAFTAndTechSpecifications
+INSTEAD OF INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        JOIN AIRCRAFT a
+            ON i.BoardNumber = a.BoardNumber
+    )
+    BEGIN
+        RAISERROR(N'Aircraft with this BoardNumber already exists.', 16, 1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END;
+
+    INSERT INTO AIRCRAFT 
+        (BoardNumber, Model, Manufacturer)
+    SELECT 
+        BoardNumber, 
+        Model, 
+        Manufacturer
+    FROM inserted;
+
+    INSERT INTO AircraftTechSpecifications
+        (AircraftID, PassengerCapacity, LoadCapacity, AircraftAge, Status)
+    SELECT
+        a.AircraftID,
+        i.PassengerCapacity,
+        i.LoadCapacity,
+        i.AircraftAge,
+        i.Status
+    FROM inserted i
+    JOIN AIRCRAFT a
+        ON a.BoardNumber = i.BoardNumber;
+END;
+GO
+
+IF OBJECT_ID(N'Trigger_Update_view_AIRCRAFTAndTechSpecifications') IS NOT NULL 
+    DROP TRIGGER Trigger_Update_view_AIRCRAFTAndTechSpecifications;
+GO
+
+CREATE TRIGGER Trigger_Update_view_AIRCRAFTAndTechSpecifications
+ON view_AIRCRAFTAndTechSpecifications
+INSTEAD OF UPDATE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM inserted
+        WHERE PassengerCapacity < 0
+           OR LoadCapacity      < 0
+    )
+    BEGIN
+        RAISERROR(N'PassengerCapacity and LoadCapacity must be positive.', 16, 1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END;
+
+    WITH UpdatedAIRCRAFT AS
+    (
+        SELECT
+            a.AircraftID,
+            i.BoardNumber,
+            i.Model,
+            i.Manufacturer
+        FROM AIRCRAFT a
+        INNER JOIN inserted i
+            ON a.AircraftID = i.AircraftID
+    )
+    UPDATE a
+    SET
+        a.BoardNumber  = ua.BoardNumber,
+        a.Model        = ua.Model,
+        a.Manufacturer = ua.Manufacturer
+    FROM AIRCRAFT a
+    INNER JOIN UpdatedAircraft ua
+        ON a.AircraftID = ua.AircraftID;
+
+    WITH UpdatedTechSpecifications AS
+    (
+        SELECT
+            ts.AircraftID,
+            i.PassengerCapacity,
+            i.LoadCapacity,
+            i.AircraftAge,
+            i.Status
+        FROM AircraftTechSpecifications ts
+        INNER JOIN inserted i
+            ON ts.AircraftID = i.AircraftID
+    )
+    UPDATE ts
+    SET
+        ts.PassengerCapacity = uts.PassengerCapacity,
+        ts.LoadCapacity      = uts.LoadCapacity,
+        ts.AircraftAge       = uts.AircraftAge,
+        ts.Status            = uts.Status
+    FROM AircraftTechSpecifications ts
+    INNER JOIN UpdatedTechSpecifications uts
+        ON ts.AircraftID = uts.AircraftID;
+END;
+GO
+
+IF OBJECT_ID(N'Trigger_Delete_view_AIRCRAFTAndTechSpecifications') IS NOT NULL 
+    DROP TRIGGER Trigger_Delete_view_AIRCRAFTAndTechSpecifications;
+GO
+
+CREATE TRIGGER Trigger_Delete_view_AIRCRAFTAndTechSpecifications
+ON view_AIRCRAFTAndTechSpecifications
+INSTEAD OF DELETE
+AS
+BEGIN
+    DELETE FROM AircraftTechSpecifications
+    WHERE AircraftID IN (
+    SELECT AircraftID 
+    FROM deleted);
+
+    DELETE FROM AIRCRAFT
+    WHERE AircraftID IN (
+    SELECT AircraftID 
+    FROM deleted);
+END;
+GO
+
+-- Tests:
+INSERT INTO view_AIRCRAFTAndTechSpecifications
+    (BoardNumber, Model, Manufacturer, PassengerCapacity, LoadCapacity, AircraftAge, Status)
+VALUES
+    ('L9000001', 'A321', 'Airbus', 220, 18.50, 3, 1),
+    ('B9000002', 'B787', 'Boeing', 260, 22.00, 2, 2);
+GO
+
+SELECT * FROM view_AIRCRAFTAndTechSpecifications;
+GO
+
+SELECT * FROM AIRCRAFT;
+GO
+
+SELECT * FROM AircraftTechSpecifications;
+GO
+
+--invalid
+--INSERT INTO view_AIRCRAFTAndTechSpecifications
+--    (BoardNumber, Model, Manufacturer, PassengerCapacity, LoadCapacity, AircraftAge, Status)
+--VALUES
+--    ('L9000001', 'A330', 'Airbus', 250, 20.00, 4, 1);
+--PRINT 'Error (duplicate INSERT): ' + ERROR_MESSAGE();
+--GO
+
+UPDATE view_AIRCRAFTAndTechSpecifications
+SET 
+    Model = 'A321neo',
+    PassengerCapacity = 230,
+    LoadCapacity = 19.75
+WHERE BoardNumber = 'L9000001';
+GO
+
+SELECT * FROM view_AIRCRAFTAndTechSpecifications;
+GO
+
+SELECT * FROM AIRCRAFT;
+GO
+
+SELECT * FROM AircraftTechSpecifications;
+GO
+
+--invalid
+--UPDATE view_AIRCRAFTAndTechSpecifications
+--SET PassengerCapacity = -10      -- триггер должен выдать RAISERROR
+--WHERE BoardNumber = 'B9000002';
+--PRINT 'Error (invalid UPDATE): ' + ERROR_MESSAGE();
+--GO
+
+SELECT * FROM AircraftTechSpecifications;
+GO
+
+DELETE FROM view_AIRCRAFTAndTechSpecifications
+WHERE BoardNumber = 'L9000001';
+GO
+
+SELECT * FROM view_AIRCRAFTAndTechSpecifications;
+GO
+
+SELECT * FROM AIRCRAFT;
+GO
+
+SELECT * FROM AircraftTechSpecifications;
+GO
